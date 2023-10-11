aa0af1adfe90e01dd43a86501a894a4c348210e9
Gazmend Nikqi <gazmendn93@gmail.com>
2023-10-11 13:33:47 +0200

    Added navigation links and routes to pages

--- js-frameworks-ca/src/App.js
+++ js-frameworks-ca/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Home from './Home';
+import Product from './Product';
+import Cart from './Cart';
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <ul>
-        <li>
-           Home 
-        </li>
-        <li>
-           Product
-        </li><li>
-           Cart 
-        </li><li>
-           Checkout 
-        </li>
-       </ul>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Product">Product</Link>
+              </li>
+              <li>
+                <Link to="/Cart">Cart</Link>
+              </li>
+              <li>
+                <Link to="/Checkout">Checkout</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>      
+            <Route path="/" exact component={Home} />
+            <Route path="/Product" component={Product} />
+            <Route path="/Cart" component={Cart} />
+            <Route path="/Checkout" component={Checkout} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
