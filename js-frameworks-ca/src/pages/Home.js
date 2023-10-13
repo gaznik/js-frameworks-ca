d17efdccf5b7fe5c6f6b64c19ce9cc18f802c946
Gazmend Nikqi <gazmendn93@gmail.com>
2023-10-13 11:53:52 +0200

    Fetching list of products on homepage

--- js-frameworks-ca/src/pages/Home.js
+++ js-frameworks-ca/src/pages/Home.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch('https://api.noroff.dev/api/v1/online-shop');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome. This is the homepage</p>
+      <h1>Products</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <div>
+                <img
+                  src={product.imageUrl} 
+                  alt={product.title}
+                  style={{ maxWidth: '100px' }} 
+                />
+              </div>
+              <div>
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <p>Price: {product.price} USD</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
