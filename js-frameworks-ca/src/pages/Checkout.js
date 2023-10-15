406e53fe968d8c420fcb5e3d3d1228fdd97330d6
Gazmend Nikqi <gazmendn93@gmail.com>
2023-10-15 10:14:05 +0200

    Layout style for the checkout page

--- js-frameworks-ca/src/pages/Checkout.js
+++ js-frameworks-ca/src/pages/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../css/Checkout.css'
 
 function Cart({ cartItems, removeItem, clearCart }) {
   const navigate = useNavigate();
@@ -12,11 +13,11 @@ function Cart({ cartItems, removeItem, clearCart }) {
   };
 
   return (
-    <div>
+    <div className="cart-container">
       <h1 className="page-title">My Cart</h1>
       <ul>
         {cartItems.map((item, index) => (
-          <li key={index}>
+          <li className="cart-item" key={index}>
             {item.title} - ${item.price.toFixed(2)}
             <button onClick={() => removeItem(item)} className="remove-button">Remove</button>
           </li>
