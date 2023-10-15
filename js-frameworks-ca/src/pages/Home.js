c63aa2c51ff5ca672bf4ce65d5349c3bd1cc535c
Gazmend Nikqi <gazmendn93@gmail.com>
2023-10-15 09:27:47 +0200

    Tidy up the directories

--- js-frameworks-ca/src/pages/Home.js
+++ js-frameworks-ca/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Product from './Product';
+import Product from '../Product';
 import SearchBar from '../SearchBar';
 import "../css/Home.css";
 
