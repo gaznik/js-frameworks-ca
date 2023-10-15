4a1b58d9a9004ddc1647e89e210d8acc0b67d479
Gazmend Nikqi <gazmendn93@gmail.com>
2023-10-15 09:27:12 +0200

    Footer styling

--- js-frameworks-ca/src/Footer.js
+++ js-frameworks-ca/src/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/Footer.css';
 
 function Footer() {
   return (
     <footer className="footer">
       <p>&copy; Shopshopshop</p>
+      <Link to="/contact">Contact Us</Link>
     </footer>
   );
 }
