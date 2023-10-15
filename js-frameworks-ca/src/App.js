4e70f2fe34c6e9a9645a8c9d45dec30c24893d4a
Gazmend Nikqi <gazmendn93@gmail.com>
2023-10-15 12:12:18 +0200

    Fixed Layout component. Included Header

--- js-frameworks-ca/src/App.js
+++ js-frameworks-ca/src/App.js
@@ -1,14 +1,15 @@
+// App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Layout from './Layout';
-import Header from './Header';
+import Header from './Header'; 
 import Home from './pages/Home';
 import ProductPage from './pages/ProductPage';
 import Checkout from './pages/Checkout';
 import CheckoutSuccess from './pages/CheckoutSuccess';
-import Contact from './pages/Contact'; 
+import Contact from './pages/Contact';
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -19,7 +20,7 @@ function App() {
   };
 
   const removeItem = (itemToRemove) => {
-    const updatedCart = cart.filter(item => item !== itemToRemove);
+    const updatedCart = cart.filter((item) => item !== itemToRemove);
     setCart(updatedCart);
   };
 
@@ -27,17 +28,18 @@ function App() {
     setCart([]);
   };
 
+  const headerComponent = <Header cartItemCount={cart.length} />; 
+
   return (
     <Router>
-      <Header cartItemCount={cart.length} />
-      <Layout>
+      <Layout header={headerComponent}> 
         <div className="App">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductPage onAddToCart={addToCart} />} />
             <Route path="/checkout" element={<Checkout cartItems={cart} removeItem={removeItem} clearCart={clearCart} />} />
             <Route path="/checkout-success" element={<CheckoutSuccess clearCart={clearCart} />} />
-            <Route path="/contact" element={<Contact />} /> 
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </Layout>
--- js-frameworks-ca/src/Layout.js
+++ js-frameworks-ca/src/Layout.js
@@ -1,10 +1,12 @@
+// Layout.js
 import React from 'react';
 import Footer from './Footer';
 import './css/Layout.css';
 
-function Layout({ children }) {
+function Layout({ children, header }) {
   return (
     <div className="layout">
+      {header}
       <main className="content">{children}</main>
       <Footer />
     </div>
